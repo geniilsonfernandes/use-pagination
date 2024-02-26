be07bdfa76d6785aa5f11b88f2e81bb915c4b733
Genilson fernandes <geniilsonfernandes@gmail.com>
2024-02-26 15:13:06 -0300

    feat: create hook pagination

---
+++ example/src/App.tsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import usePagination from "./hook/usePagination";
+
+function App() {
+  const { currentPage, handlePageChange, range, nextPage, prevPage } =
+    usePagination({
+      initialPage: 1,
+      totalPages: 10,
+      hiddenBoundary: true,
+    });
+
+  console.log(range);
+
+  return (
+    <div>
+      <p>Current Page: {currentPage}</p>
+      <div>
+        <button onClick={prevPage}>Previous</button>
+        {range.map((page, i) => {
+          if (page === "...") return <button key={i}>...</button>;
+          else
+            return (
+              <button
+                key={i}
+                className={currentPage === page ? "active-page" : ""}
+                onClick={() => handlePageChange(page)}
+              >
+                {page}
+              </button>
+            );
+        })}
+        <button onClick={nextPage}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
